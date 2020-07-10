ac48b2c5f91d4fd5c07d73ade71da0944b0c4368
Dennis Koch <info@pixelarbeit.de>
2020-07-10 13:39:56 +0200

    feat: add db credentials autoloading for laravel

---
+++ recipe/laravel.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Deployer;
+
+set('mysql.before_cmd', 'source {{release_path}}/.env');
+
+set('mysql.connection', [
+    'host' => '$DB_HOST',
+    'port' => '$DB_PORT',
+    'schema' => '$DB_DATABASE',
+    'username' => '$DB_USERNAME',
+    'password' => '$DB_PASSWORD',
+]);
